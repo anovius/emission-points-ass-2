b66a785795e9f3091676c1d4c9815b178cb19a2a
Usman Farooq <anovius10@gmail.com>
2021-11-09 00:52:06 +0500

    quiz activity added

--- app/src/main/java/com/example/emissionpointsquiz/Homepage.java
+++ app/src/main/java/com/example/emissionpointsquiz/Homepage.java
@@ -2,7 +2,9 @@ package com.example.emissionpointsquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Homepage extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+    }
+
+    public void  startQuiz(View view){
+        Intent next = new Intent(this, QuizActivity.class);
+        startActivity(next);
     }
 }
