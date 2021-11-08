708cd15f6546c66d038c5052caad245e94f881f8
Usman Farooq <anovius10@gmail.com>
2021-11-08 23:55:07 +0500

    question class added

---
+++ app/src/main/java/com/example/emissionpointsquiz/Question.java
@@ -0,0 +1,10 @@
+package com.example.emissionpointsquiz;
+
+import java.util.List;
+
+public class Question {
+    String statement;
+    List<String> options;
+    int correctOption;
+    int selectedOption;
+}
