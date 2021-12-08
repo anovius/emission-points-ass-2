ef5db1df28390873732a8a9f589f9609160e0e3d
Usman Farooq <anovius10@gmail.com>
2021-12-08 06:36:56 +0500

    app bar added

--- app/src/main/java/com/example/emissionpointsquiz/MainActivity.java
+++ app/src/main/java/com/example/emissionpointsquiz/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.emissionpointsquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,15 +17,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
-    public void nextActivity(View view){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.app_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.github:
+                gotoGitHub();
+                return true;
+            case R.id.startMenu:
+                nextActivity();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void nextActivity(){
         Intent next = new Intent(this, Homepage.class);
         startActivity(next);
     }
 
 
-    public  void gotoGitHub(View view){
+    public  void gotoGitHub(){
         Uri webpage = Uri.parse("https://github.com/anovius/emission-points-ass-2");
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
         startActivity(intent);
